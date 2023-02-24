d6591a3a0b5506b28a7abb84bb142d693db0c11a
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-02-24 16:04:24 -0300

    primera vez

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const form1 = document.getElementById("form1");
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const creditCardNumber = document.getElementById("textTarjeta").value;
+  document.getElementById("ok").innerHTML =
+    "Tus números de tarjeta son " + creditCardNumber;
+  //console.log(typeof creditCardNumber, creditCardNumber);
+
+  const validar = validator.isValid(creditCardNumber);
+  //console.log(validar);
+  const mascara = validator.maskify(creditCardNumber);
+  if (validar === true) {
+    document.getElementById("ok").innerHTML = "Tu tarjeta es válida " + mascara;
+  } else {
+    document.getElementById("ok").innerHTML =
+      "Tu tarjeta NO es válida" + mascara;
+  }
+});
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,74 @@
 const validator = {
-  // ...
-};
+  //...
+  
+  //`validator.isValid(creditCardNumber)`: `creditCardNumber` es un `string`
+  //con el número de tarjeta que se va a verificar. Esta función debe retornar
+  //un `boolean` dependiendo si es válida de acuerdo al [algoritmo de Luhn](https://es.wikipedia.org/wiki/Algoritmo_de_Luhn).
+  isValid(num){
+    const arreglo = num.split('');
+    //console.log(arreglo);
+    const parMul = [];
+    //console.log(parMul);
+    const imPar= [];
+    //console.log(imPar);
+    
+    //*---------sacar par,multiplicar y sumar multiplo
+    for(let i = 1; i <= arreglo.length-1; i = i + 2){
+      let par= [];
+      par = (arreglo[i] * 2);
+    
 
+      if (par >= 10){
+        //console.log(par - 9);
+        parMul.push(par - 9);
+      }else{
+        //console.log(par);
+        parMul.push(par);
+      }
+    }  
+    //*----------sacar impar
+    for(let i = 0; i < arreglo.length; i = i + 2){
+      //console.log(arreglo[i]);
+      let impar= [];
+      impar = Number(arreglo[i]);
+      //console.log(impar);
+      imPar.push(impar);
+    }
+    //*----------sumar pares
+    const valor= 0;
+    //console.log(parMul);
+    const sumaPar = parMul.reduce((accumulator,currentValue) => accumulator + currentValue,valor);
+    //console.log(sumaPar);
+    //*---------sumar impares
+    const sumaIm= imPar.reduce((accumulator,currentValue) => accumulator + currentValue,valor);
+    //console.log(sumaIm + sumaPar);
+    //*---------sumar ambos
+    const numerosJuntos=[];
+    numerosJuntos.push(sumaPar);
+    numerosJuntos.push(sumaIm);
+    //console.log(numerosJuntos);
+    const sumados= numerosJuntos.reduce((accumulator,currentValue) => accumulator + currentValue,valor);
+    // console.log(sumados);
+
+    if(sumados % 10 === 0){
+      return true
+    }else{
+      return false
+    }
+
+  },
+
+  //- `validator.maskify(creditCardNumber)`: `creditCardNumber` es un `string` con
+  //el número de tarjeta y esta función debe retornar un `string` donde todos menos
+  //los últimos cuatro caracteres sean reemplazados por un numeral (`#`) o 🐱.
+  //Esta función deberá siempre mantener los últimos cuatro caracteres
+  //intactos, aún cuando el `string` sea de menor longitud.
+  maskify(num){
+    const trae= num;
+    //console.log(typeof trae, trae);
+    const enmascarar= trae.slice(-4).padStart(trae.length,'#');
+    return enmascarar;
+  }
+}
+    
 export default validator;
